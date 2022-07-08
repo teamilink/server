10a74bda1716edcf2d9f19910acaf770e3b1e9d9
aanmeba <aanmeba@gmail.com>
2022-07-08 17:17:09 +1000

    create create and login methods for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :links
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/signup', to: 'users#create'
+  post '/login', to: 'users#login'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_040201) do
+ActiveRecord::Schema.define(version: 2022_07_08_061118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2022_07_08_040201) do
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "link_address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
