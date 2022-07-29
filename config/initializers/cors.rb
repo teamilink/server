2b80c10d33eeb0f33d8b315c4fc8a3acd91036cf
aanmeba <aanmeba@gmail.com>
2022-07-29 20:48:48 +1000

    add additional port for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ilinkapp.netlify.app', 'http://localhost:3000'
+    origins 'https://ilinkapp.netlify.app', 'http://localhost:3000', 'http://localhost:3001'
 
     resource '*',
       headers: :any,
