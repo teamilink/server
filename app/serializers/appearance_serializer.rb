a69f34da7c701cfa42a36402cd6c0f528b7775fe
aanmeba <aanmeba@gmail.com>
2022-07-29 14:30:49 +1000

    working on - not uploading picture throws error

--- app/serializers/appearance_serializer.rb
+++ app/serializers/appearance_serializer.rb
@@ -2,7 +2,7 @@ class AppearanceSerializer
   include JSONAPI::Serializer
   attributes :id, :profile_title, :bio, :bg_color, :bg_image_url, :created_at, :updated_at, :picture_url
 
-  # def picture_url
+  # def picture
   #   picture.blob.url if picture.attached?
   # end
 end
