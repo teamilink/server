caaee4e8888016c0e4acebc215b793d8551084cf
aanmeba <aanmeba@gmail.com>
2022-08-06 01:48:16 +1000

    add timestamps for random image and attachment

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -14,11 +14,39 @@ class AppearancesController < ApplicationController
 
   # PATCH/PUT /appearance/1
   def update
-    if @appearance.update(appearance_params)
-      render json: @appearance
-    else
-      render json: @appearance.errors.full_messages, status: :unprocessable_entity
-    end
+
+    pp "***************************************************"
+    pp appearance_params
+    pp "***************************************************"
+
+    # 1. picture is uploaded
+    # 1-1. then random image is added
+    # 1-2. then picture is uploaded
+    # 2. random image is added
+    # 2-1. then a picture is uploaded
+    # 2-2. then random image is added
+
+    # if picture was attached, and a new bg_image is added
+    # remove the existing picture
+    # if @appearance.picture && appearance_params[:bg_image_url]
+    #   pp "********************** if *****************************"
+    #   @appearance.picture.destroy
+    #   @appearance.update(appearance_params)
+    #   render json: @appearance
+    # elsif @appearance.bg_image_url && appearance_params[:picture]
+    #   pp "*********************** elsif ****************************"
+    #   @appearance.bg_image_url = nil
+    #   @appearance.save
+    #   @appearance.update(appearance_params)
+    #   render json: @appearance
+    # else
+    #   pp "*********************** else ****************************"
+      if @appearance.update(appearance_params)
+        render json: @appearance
+      else
+        render json: @appearance.errors.full_messages, status: :unprocessable_entity
+      end
+    # end
   end
 
   # DELETE /appearance/1
@@ -35,6 +63,6 @@ class AppearancesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def appearance_params
-      params.require(:appearance).permit(:user_id, :profile_title, :bio, :bg_color, :bg_image_url, :picture, :picture_url, :id).select {|x, v| v.present?}
+      params.require(:appearance).permit(:user_id, :profile_title, :bio, :bg_color, :bg_image_url, :picture, :picture_url, :img_timestamp, :pic_timestamp, :id).select {|x, v| v.present?}
     end
 end
--- app/serializers/appearance_serializer.rb
+++ app/serializers/appearance_serializer.rb
@@ -1,4 +1,4 @@
 class AppearanceSerializer
   include JSONAPI::Serializer
-  attributes :id, :profile_title, :bio, :bg_color, :bg_image_url, :picture_url
+  attributes :id, :profile_title, :bio, :bg_color, :bg_image_url, :picture_url, :pic_timestamp, :img_timestamp
 end
