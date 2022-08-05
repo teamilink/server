cbc70c19ab5c191733eb2c9bcb80c7faa20d23cb
aanmeba <aanmeba@gmail.com>
2022-08-05 20:49:35 +1000

    remove unused properties

--- app/serializers/appearance_serializer.rb
+++ app/serializers/appearance_serializer.rb
@@ -1,4 +1,4 @@
 class AppearanceSerializer
   include JSONAPI::Serializer
-  attributes :id, :profile_title, :bio, :bg_color, :bg_image_url, :uploaded_picture_url, :picture_url
+  attributes :id, :profile_title, :bio, :bg_color, :bg_image_url, :picture_url
 end
