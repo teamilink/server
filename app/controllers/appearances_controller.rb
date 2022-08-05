16a60c58d4742a1cc3f48082e1d2f84ff27a7da5
aanmeba <aanmeba@gmail.com>
2022-08-06 01:50:01 +1000

    clean up

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -14,39 +14,11 @@ class AppearancesController < ApplicationController
 
   # PATCH/PUT /appearance/1
   def update
-
-    pp "***************************************************"
-    pp appearance_params
-    pp "***************************************************"
-
-    # 1. picture is uploaded
-    # 1-1. then random image is added
-    # 1-2. then picture is uploaded
-    # 2. random image is added
-    # 2-1. then a picture is uploaded
-    # 2-2. then random image is added
-
-    # if picture was attached, and a new bg_image is added
-    # remove the existing picture
-    # if @appearance.picture && appearance_params[:bg_image_url]
-    #   pp "********************** if *****************************"
-    #   @appearance.picture.destroy
-    #   @appearance.update(appearance_params)
-    #   render json: @appearance
-    # elsif @appearance.bg_image_url && appearance_params[:picture]
-    #   pp "*********************** elsif ****************************"
-    #   @appearance.bg_image_url = nil
-    #   @appearance.save
-    #   @appearance.update(appearance_params)
-    #   render json: @appearance
-    # else
-    #   pp "*********************** else ****************************"
-      if @appearance.update(appearance_params)
-        render json: @appearance
-      else
-        render json: @appearance.errors.full_messages, status: :unprocessable_entity
-      end
-    # end
+    if @appearance.update(appearance_params)
+      render json: @appearance
+    else
+      render json: @appearance.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   # DELETE /appearance/1
